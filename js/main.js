fe831745e617a3d65e73885002ee711500dfdf27
jessica <trabalhojaime2019@gmail.com>
2019-12-06 00:18:35 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$('.slider-principal').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: false,
+    autoplay: true,
+    autoplaySpeed: 1800
+    
+  });
